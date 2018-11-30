2d00088ef275c127d33c8b5e298a97fe0add1cdb
Efua <kagyarek@u.rochester.edu>
2018-11-29 20:49:00 -0500

    content creating

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>The Beauty of the Classics</title>
+  </head>
+
+  <body>
+
+    <h1>Clean. Ordered. Genteel.</h1>
+
+    <h2>Discover the origin of Class today</h2>
+
+    <section>
+      <p>When people say "classical music," they usually mean anything you would hear an Eastman student play. But Western music of itself had a classial period, with its own distinctions, complexities and herioc performers.</p> 
+
+      <p>The dates of the classical period in Western music are generally accepted as being between 1750 and 1820. It falls right between the Baroque and Romantic periods. </p>  
+
+      <p>During the era, music moved away from thicker, more complex textures to a more simplistic format. Classical music has a lighter, clearer texture than baroque music and is less complex. It is mainly homophonic—melody above chordal accompaniment (but counterpoint by no means is forgotten, especially later in the period). It also makes use of style galant in the classical period which was drawn in opposition to the strictures of the baroque style, emphasizing light elegance in place of the baroque’s dignified seriousness and impressive grandeur.</p>
+
+    </section>
+
+    <section> Learn more about the grand masters of the Classical Era. </section>
+
+  </body>
+
+</html>
---
+++ mozart.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>The Beauty of the Classics</title>
+  </head>
+
+  <body>
+
+    <h1>Wolfgang Amadeus Mozart (1756-1791)</h1>
+
+    <h2>The Prodigy</h2>
+
+    <section>
+      <p>Did you know that nearly half of Mozart's life was spent touring the European continent? Born in 1756, Mozart was a musical prodigy who began composing at the age of five. Shortly after his talent was discovered, his father was quick to take him on tours with his sister. Tragically, Mozart died at the young age of 35. Despite his short life, Mozart greatly advanced classical period music, contributing over 600 compositions. His compositional style is similar to that of Haydn's, only more flamboyant and, during his lifetime, often criticized for having "too many notes."</p> 
+
+    </section>
+
+    <section> Learn more about the grand masters of the Classical Era. </section>
+
+  </body>
+
+</html>
