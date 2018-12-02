ea0d654bc6fec5e94d6d7eeb23ca6661bc3d38c7
Masa <mkawaba2@u.rochester.edu>
2018-12-02 12:52:55 -0500

    content edit

--- index.php
+++ index.php
@@ -1,44 +1,26 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>The Beauty of the Classics</title>
-    <link rel="stylesheet" href="css/navigation.css">
-    <link rel="stylesheet" href="css/styles.css">
-  </head>
+<?php
+include "inc/html-top.inc"
+?> 
 
   <body>
 
-  	<nav>
-  		<ul>
-  			<li><a href="index.php">Home</a></li>
-  			<li><a href="bach.php">Johann Bach</a></li>
-  			<li><a href="beethoven.php">Ludwig Beethoven</a></li>
-  			<li><a href="haydn.php">Franz Haydn</a></li>
-  			<li><a href="mozart.php">Wolfgang Mozart</a></li>
-  		</ul>
-  	</nav>
+  	<section>
+  		<h3 class="fact">Famous Classical Music Composers</h3>
+  	</section>
 
-    <figure>
-      <img src="images/piano-playing.jpg">
-      <img src="images/piano-playing2.jpg">
-      <figcaption>It's a Classic! (Literally)</figcaption>
-    </figure>
+  	<div class="quote">
+	    <h1>Clean. Ordered. Genteel.</h1>
 
-    <h1>Clean. Ordered. Genteel.</h1>
+	    <h2>Discover the origin of classics today</h2>
+	</div>
 
-    <h2>Discover the origin of Class today</h2>
+	<section>
+		<h3 class="curious">Want to learn more about the composers?</h3>
+	</section>
 
-    <section>
-      <p>When people say "classical music," they usually mean anything you would hear an Eastman student play. But Western music of itself had a classial period, with its own distinctions, complexities and herioc performers.</p> 
-
-      <p>The dates of the classical period in Western music are generally accepted as being between 1750 and 1820. It falls right between the Baroque and Romantic periods. </p>  
-
-      <p>During the era, music moved away from thicker, more complex textures to a more simplistic format. Classical music has a lighter, clearer texture than baroque music and is less complex. It is mainly homophonic—melody above chordal accompaniment (but counterpoint by no means is forgotten, especially later in the period). It also makes use of style galant in the classical period which was drawn in opposition to the strictures of the baroque style, emphasizing light elegance in place of the baroque’s dignified seriousness and impressive grandeur.</p>
-
-    </section>
-
-    <section> Learn more about the grand masters of the Classical Era. </section>
+	<section>
+		<h3><a href="bach.php" class="start">Click here to get started!</a></h3>
+	</section>
 
   </body>
 
