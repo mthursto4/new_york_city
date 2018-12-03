0d89387630ff0f77cb718619f37532bd383acc7e
Masa <mkawaba2@u.rochester.edu>
2018-12-03 10:43:56 -0500

    Basic css/content edit

--- beethoven.php
+++ beethoven.php
@@ -1,26 +1,29 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Beethoven</title>
-  </head>
+<?php
+include "inc/html-top.inc"
+?> 
 
   <body>
+
+  <?php 
+  include "inc/nav.inc"
+  ?>
+
+
     <figure>
       <img src="beethoven.jpeg">
       <figcaption>Portrait of Beethoven</figcaption>
     </figure>
 
-    <h1>Ludwig van Beethoven (1770-1827)</h1>
-
-    <h2>The Renegade</h2>
-
     <section>
-      <p>Many view Beethoven as the bridge connecting the classical period to the romantic period. Beethoven only wrote nine symphonies. Compare that to Haydn and Mozart, who, combined, wrote over 150 symphonies. What makes Beethoven so special? It was Beethoven's successful attempt to break the mold of the highly structured and refined rules of classical period composition. His compositions, especially the famous Symphony No. 9, opened the flood gates of composing with emotional abandon.</p> 
+
+      <h1>Ludwig van Beethoven (1770-1827)</h1>
+
+      <h2>The Renegade</h2>
+
+
+        <p>Many view Beethoven as the bridge connecting the classical period to the romantic period. Beethoven only wrote nine symphonies. Compare that to Haydn and Mozart, who, combined, wrote over 150 symphonies. What makes Beethoven so special? It was Beethoven's successful attempt to break the mold of the highly structured and refined rules of classical period composition. His compositions, especially the famous Symphony No. 9, opened the flood gates of composing with emotional abandon.</p> 
 
     </section>
-
-    <section> Learn more about the grand masters of the Classical Era. </section>
 
   </body>
 
--- haydn.php
+++ haydn.php
@@ -1,20 +1,24 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Haydn</title>
-  </head>
+<?php
+include "inc/html-top.inc"
+?> 
 
   <body>
+
+    <?php 
+    include "inc/nav.inc"
+    ?>
+
     <figure>
       <img src="haydn.jpeg">
       <figcaption>Portrait of Haydn</figcaption>
     </figure>
 
+    <section>
+
     <h1>Franz Joseph Haydn (1732-1809)</h1>
+
     <h2>Father of the Symphony</h2>
 
-    <section>
       <p>Haydn was a remarkable composer, epitomizing the meaning of classical period composition, and his music always stayed true to form. Haydn, unlike most composers, had a "reliable and steady" job composing, directing, teaching, performing, and managing musicians from the royal Esterhazy family. During this time, Haydn composed many pieces of music for the courtly orchestra to perform. With a staggering body of work, including over 100 symphonies and 60 string quartets, he is often referred to as the "Father of the Symphony" or "Father of the String Quartet."</p> 
 
     </section>
