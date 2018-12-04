e6905d1babf89fa5b97cb23259a54b023be40558
Masa <mkawaba2@u.rochester.edu>
2018-12-03 19:24:02 -0500

    blah

---
+++ haydn.php
@@ -0,0 +1,30 @@
+<?php
+include "inc/html-top.inc"
+?> 
+
+  <body>
+
+    <?php 
+    include "inc/nav.inc"
+    ?>
+
+    <figure>
+      <img src="images/haydn.jpeg">
+      <figcaption>Portrait of Haydn</figcaption>
+    </figure>
+
+    <section>
+
+    <h1>Franz Joseph Haydn (1732-1809)</h1>
+
+    <h2>Father of the Symphony</h2>
+
+      <p>Haydn was a remarkable composer, epitomizing the meaning of classical period composition, and his music always stayed true to form. Haydn, unlike most composers, had a "reliable and steady" job composing, directing, teaching, performing, and managing musicians from the royal Esterhazy family. During this time, Haydn composed many pieces of music for the courtly orchestra to perform. With a staggering body of work, including over 100 symphonies and 60 string quartets, he is often referred to as the "Father of the Symphony" or "Father of the String Quartet."</p> 
+
+    </section>
+
+    <section> Learn more about the grand masters of the Classical Era. </section>
+
+  </body>
+
+</html>
