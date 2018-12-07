18b845edd5d7c98dc2136de60e459d38d4a864b6
Margaret Thurston <mthursto@u.rochester.edu>
2018-12-07 01:08:45 -0500

    added survey results placeholder

--- admin/welcome.php
+++ admin/welcome.php
@@ -27,6 +27,12 @@ include "../inc/head.php"
     <div class="page-header">
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
     </div>
+
+    <div>
+        <h2>Survey Results</h2>
+        <p>[insert table here]</p>
+    </div>
+
     <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
