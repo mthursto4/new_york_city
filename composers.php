26e113a0c1ccaf9469128b65bc847458983f6ff7
Masa <mkawaba2@u.rochester.edu>
2018-12-09 11:18:13 -0500

    content edit

--- composers.php
+++ composers.php
@@ -11,58 +11,56 @@
   include "inc/nav.php"
   ?>
   
-  <section class="fpattern" id="bach">
+  <section class="fpattern">
 
-    <figure>
-      <img src="images/bach.jpg">
-    </figure>
+      <figure>
+        <img src="images/bach.jpg">
+      </figure>
 
-      <h1 class="name">Carl Philipp Emanuel Bach (1714-1788)</h1>
+        <h1 class="name">Carl Philipp Emanuel Bach (1714-1788)</h1>
 
-      <h2 class="sub">Founding Father</h2>
+        <h2 class="sub">Founding Father</h2>
 
-         <p class="details">The second of three sons born to the great composer, Johann Sebastian Bach, Carl Philipp Emanuel Bach (named in part to honor Georg Philipp Telemann, Bach Sr.'s friend and C.P.E. Bach's godfather), was highly revered by Mozart, Haydn, and Beethoven. C.P.E. Bach's most valuable contribution to the classical period (and the musical world as a whole) was his publication, An Essay on the True Art of Playing Keyboard Instruments. It instantly became the definitive for piano technique. To this day, it is still largely taught throughout the world.</p> 
+           <p class="details">The second of three sons born to the great composer, Johann Sebastian Bach, Carl Philipp Emanuel Bach (named in part to honor Georg Philipp Telemann, Bach Sr.'s friend and C.P.E. Bach's godfather), was highly revered by Mozart, Haydn, and Beethoven. C.P.E. Bach's most valuable contribution to the classical period (and the musical world as a whole) was his publication, An Essay on the True Art of Playing Keyboard Instruments. It instantly became the definitive for piano technique. To this day, it is still largely taught throughout the world.</p> 
 
-  </section>
+      <figure>
+        <img src="images/beethoven.jpg">
+      </figure>
 
-  <section class="fpattern" id="beet">
+        <h1 class="name">Ludwig van Beethoven (1770-1827)</h1>
 
-    <figure>
-      <img src="images/beethoven.jpg">
-    </figure>
-
-      <h1>Ludwig van Beethoven (1770-1827)</h1>
-
-      <h2>The Renegade</h2>
+        <h2 class="sub">The Renegade</h2>
 
 
-        <p>Many view Beethoven as the bridge connecting the classical period to the romantic period. Beethoven only wrote nine symphonies. Compare that to Haydn and Mozart, who, combined, wrote over 150 symphonies. What makes Beethoven so special? It was Beethoven's successful attempt to break the mold of the highly structured and refined rules of classical period composition. His compositions, especially the famous Symphony No. 9, opened the flood gates of composing with emotional abandon.</p>
+          <p class="details">Many view Beethoven as the bridge connecting the classical period to the romantic period. Beethoven only wrote nine symphonies. Compare that to Haydn and Mozart, who, combined, wrote over 150 symphonies. What makes Beethoven so special? It was Beethoven's successful attempt to break the mold of the highly structured and refined rules of classical period composition. His compositions, especially the famous Symphony No. 9, opened the flood gates of composing with emotional abandon.</p>
+
+      <figure>
+        <img src="images/haydn.jpg">
+      </figure>
+
+      <h1 class="name">Franz Joseph Haydn (1732-1809)</h1>
+
+      <h2 class="sub">Father of the Symphony</h2>
+
+        <p class="details">Haydn was a remarkable composer, epitomizing the meaning of classical period composition, and his music always stayed true to form. Haydn, unlike most composers, had a "reliable and steady" job composing, directing, teaching, performing, and managing musicians from the royal Esterhazy family. During this time, Haydn composed many pieces of music for the courtly orchestra to perform. With a staggering body of work, including over 100 symphonies and 60 string quartets, he is often referred to as the "Father of the Symphony" or "Father of the String Quartet."</p> 
+
+        <figure>
+         <img src="images/mozart.jpg">
+        </figure>
+
+        <h1 class="name">Wolfgang Amadeus Mozart (1756-1791)</h1>
+
+        <h2 class="sub">The Prodigy</h2>
+
+         <p class="details">Did you know that nearly half of Mozart's life was spent touring the European continent? Born in 1756, Mozart was a musical prodigy who began composing at the age of five. Shortly after his talent was discovered, his father was quick to take him on tours with his sister. Tragically, Mozart died at the young age of 35. Despite his short life, Mozart greatly advanced classical period music, contributing over 600 compositions. His compositional style is similar to that of Haydn's, only more flamboyant and, during his lifetime, often criticized for having "too many notes."</p>
+
+        <footer  class="source">
+            <a href="https://www.thoughtco.com/top-classical-period-composers-724098">Source: ThoughtCo, Top Classical Composers</a>
+         </footer>
 
   </section> 
 
-    <figure>
-      <img src="images/haydn.jpg">
-    </figure>
 
-    <h1>Franz Joseph Haydn (1732-1809)</h1>
-
-    <h2>Father of the Symphony</h2>
-
-      <p>Haydn was a remarkable composer, epitomizing the meaning of classical period composition, and his music always stayed true to form. Haydn, unlike most composers, had a "reliable and steady" job composing, directing, teaching, performing, and managing musicians from the royal Esterhazy family. During this time, Haydn composed many pieces of music for the courtly orchestra to perform. With a staggering body of work, including over 100 symphonies and 60 string quartets, he is often referred to as the "Father of the Symphony" or "Father of the String Quartet."</p> 
-
-    <figure>
-      <img src="images/mozart.jpg">
-    </figure>
-
-    <h1>Wolfgang Amadeus Mozart (1756-1791)</h1>
-
-    <h2>The Prodigy</h2>
-
-      <p>Did you know that nearly half of Mozart's life was spent touring the European continent? Born in 1756, Mozart was a musical prodigy who began composing at the age of five. Shortly after his talent was discovered, his father was quick to take him on tours with his sister. Tragically, Mozart died at the young age of 35. Despite his short life, Mozart greatly advanced classical period music, contributing over 600 compositions. His compositional style is similar to that of Haydn's, only more flamboyant and, during his lifetime, often criticized for having "too many notes."</p> 
-
-    <footer>
-      <a href="https://www.thoughtco.com/top-classical-period-composers-724098">Source: ThoughtCo, Top Classical Composers</a>
-    </footer>
 
   </body>
 
