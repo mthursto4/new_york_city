f5097f885299c9c4f67f094c63a7fc615576c4c1
Efua <kagyarek@u.rochester.edu>
2018-12-10 16:52:42 -0500

    more questions

--- sign-up.php
+++ sign-up.php
@@ -34,6 +34,53 @@
             </div>
           </fieldset>
 
+           <fieldset class="user">
+            <legend>User Background</legend>
+            <h3>How Would you Rate your Knowledge of Classical Music? </h3>
+            <div>
+              <label for="knowledge1">One (lowest) </label>
+              <input type="radio" name="knowledge" id="knowledge1" value="1">
+            </div>
+            <div>
+              <label for="knowledge2">Two</label>
+              <input type="radio" name="knowledge" id="knowledge2" value="2">
+            </div>
+            <div>
+              <label for="knowledge3">Three</label>
+              <input type="radio" name="knowledge" id="knowledge3" value="3">
+            </div>
+            <div>
+              <label for="knowledge4">Four</label>
+              <input type="radio" name="knowledge" id="knowledge4" value="4">
+            </div>
+            <div>
+              <label for="knowledge5">Five (highest) </label>
+              <input type="radio" name="knowledge" id="knowledge5" value="5">
+            </div>
+
+            <h3>How Would you Rate your Interest in Classical Music? </h3>
+            <div>
+              <label for="interest1">One (lowest) </label>
+              <input type="radio" name="interest" id="interest1" value="1">
+            </div>
+            <div>
+              <label for="interest2">Two</label>
+              <input type="radio" name="interest" id="interest2" value="2">
+            </div>
+            <div>
+              <label for="interest3">Three</label>
+              <input type="radio" name="interest" id="interest3" value="3">
+            </div>
+            <div>
+              <label for="interest4">Four</label>
+              <input type="radio" name="interest" id="interest4" value="4">
+            </div>
+            <div>
+              <label for="interest5">Five (highest) </label>
+              <input type="radio" name="interest" id="interest5" value="5">
+            </div>
+            </fieldset>
+
           <fieldset class="user">
             <legend>User Feedback</legend>
             <h3>Did you find our information useful? </h3>
@@ -62,8 +109,10 @@
               <label for="recommendmaybe">Maybe</label>
               <input type="radio" name="recommend" id="recommendmaybe" value="Maybe">
             </div>
+          </fieldset>
 
-            <legend>Have Your Say</legend>
+          <fieldset class="user">
+            <legend>Help Us Help You</legend>
             <h3>What era would you like us to cover next?</h3>
             <div>
               <label for="baroque">Baroque</label>
@@ -77,15 +126,12 @@
               <label for="neoclassical">NeoClassical</label>
               <input type="radio" name="era" id="neoclassical" value="NeoClassical">
             </div>
-          </fieldset>
-  
-          <fieldset class="user">
-            <legend>Help Us Help You</legend>
             <h3>What else would you like us to see? Specific musicians or pieces? Let us know how to improve!</h3>
             <div>
               <textarea name="suggestions" id="suggestions"></textarea>
             </div>
           </fieldset>
+
 
           <div class="user">
             <input type="submit" value="You're done! Submit!">
