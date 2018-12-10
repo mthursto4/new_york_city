fda6065da340ac3672d54e5155eee69ab2039b98
Margaret Thurston <mthursto@u.rochester.edu>
2018-12-10 16:11:39 -0500

    Updated with new database

--- admin/config.php
+++ admin/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'lab13');
-define('DB_PASSWORD', 'coffee');
-define('DB_NAME', 'lab13');
+define('DB_SERVER', '66.147.242.186');
+define('DB_USERNAME', 'urcscon3_nyc');
+define('DB_PASSWORD', 'liberty');
+define('DB_NAME', 'urcscon3_nyc');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
