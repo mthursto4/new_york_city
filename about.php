c60e509634c774b324992dd0f4e802daac9c7670
Efua <kagyarek@u.rochester.edu>
2018-12-10 15:51:55 -0500

    switched about page

--- about.php
+++ about.php
@@ -14,6 +14,10 @@
 
     <h1 class="title">Music Madness</h1>
 
+    <h2 class="maker">What We Do</h2>
+
+    <p class="paragraph">We are musical nerds who listen to Debussy, Bach and Beethoven the way you probably listen to Taylor Swift. In one way or another, classical music saved each of us and now we can only hope to help it do the same for others. Every week, we will be in your inbox with fascinating tidbits of a musical era, of a spotlight on a composer or player. We hope that as you learn more about the wise, talented musicians who lived, you will see that greatness lies within you too. </p>
+
     <h2 class="maker">Meet the Makers:</h2>
 
     <h3 class="name">Efua Agyare-Kumi</h3>
@@ -38,10 +42,6 @@
         <li>Favorite composer: Beethoven</li>
         <li>Favorite musical era: Romantic</li>
       </ul>
-
-    <h2 class="maker">What We Do</h2>
-
-    <p class="paragraph">We are musical nerds who listen to Debussy, Bach and Beethoven the way you probably listen to Taylor Swift. In one way or another, classical music saved each of us and now we can only hope to help it do the same for others. Every week, we will be in your inbox with fascinating tidbits of a musical era, of a spotlight on a composer or player. We hope that as you learn more about the wise, talented musicians who lived, you will see that greatness lies within you too. </p>
     
     </section>
 
