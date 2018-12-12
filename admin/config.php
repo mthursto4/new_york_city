59f5a5d43028b3f7f485e43429b0ee9d55593ca0
Margaret Thurston <mthursto@u.rochester.edu>
2018-12-12 13:56:43 -0500

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -6,9 +6,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', '66.147.242.186');
-define('DB_USERNAME', 'urcscon3_nyc');
+define('DB_USERNAME', 'urcscon3_newyork');
 define('DB_PASSWORD', 'liberty');
-define('DB_NAME', 'urcscon3_nyc');
+define('DB_NAME', 'urcscon3_newyork');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
