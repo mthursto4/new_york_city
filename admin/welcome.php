5e9cd97f61635825d8bbf163bc124ecb4a51678c
Margaret Thurston <mthursto@u.rochester.edu>
2018-12-12 11:50:54 -0500

    added table code to welcome page
    (The table is not yet existent in the database.)

--- admin/welcome.php
+++ admin/welcome.php
@@ -34,7 +34,40 @@ include "../inc/head.php"
 
     <div class="survey">
         <h2>Survey Results</h2>
-        <p>[insert table here]</p>
+
+          <table width='80%' border=0>
+              <tr bgcolor='#CCCCCC'>
+                  <td>Name</td>
+                  <td>Email</td>
+                  <td>Knowledge</td>
+                  <td>Interest</td>
+                  <td>Useful</td>
+                  <td>Recommend</td>
+                  <td>Era</td>
+                  <td>Suggestions</td>
+                  <td>Update</td>
+              </tr>
+              <?php 
+              //while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
+              while($res = mysqli_fetch_array($result)) {         
+                  echo "<tr>";
+                  echo "<td>".$res['name']."</td>";
+                  echo "<td>".$res['email']."</td>";
+                  echo "<td>".$res['knowledge']."</td>";
+                  echo "<td>".$res['interest']."</td>";
+                  echo "<td>".$res['useful']."</td>";
+                  echo "<td>".$res['recommend']."</td>";
+                  echo "<td>".$res['era']."</td>";
+                  echo "<td>".$res['suggestions']."</td>";    
+                  echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";        
+              }
+              ?>
+
+          </table>
+
+
+          <a href="add.html">Add New Data</a><br/><br/>
+
         <p>
             <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
             <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
