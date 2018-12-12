4da3fcd44a5f4bd5964148110f977787c3efa576
Margaret Thurston <mthursto@u.rochester.edu>
2018-12-12 12:24:42 -0500

    added error message to config.php

--- admin/config.php
+++ admin/config.php
@@ -9,6 +9,11 @@ define('DB_NAME', 'urcscon3_nyc');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
 
 ?>
